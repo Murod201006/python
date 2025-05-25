94cfa4219f06733dddc1b2797afd9d8a41de9005
Murod201006 <murod.nmb@gmail.com>
2025-05-25 21:15:51 +0500

    Create python.py

---
+++ lesson-15/homework/python.py
@@ -0,0 +1,19 @@
+# 1
+CREATE TABLE Roster (
+    Name TEXT,
+    Species TEXT,
+    Age INTEGER
+);
+# 2
+INSERT INTO Roster (Name, Species, Age) VALUES
+('Benjamin Sisko', 'Human', 40),
+('Jadzia Dax', 'Trill', 300),
+('Kira Nerys', 'Bajoran', 29);
+# 3
+UPDATE Roster
+SET Name = 'Ezri Dax'
+WHERE Name = 'Jadzia Dax';
+# 4
+SELECT Name, Age
+FROM Roster
+WHERE Species = 'Bajoran';
